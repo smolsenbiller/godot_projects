718613221167474230e7314ffb88f7a8f090265f
smolsenbiller <jacob.august.smith@icloud.com>
2026-02-02 21:26:44 -0800

    I did a bunch. I added a background texture, added collisions for shapes, added player movement, but most important of all I add zombie navigation. It is freaking sweet! I need to add a seperate collision for shooting the zombie but that will be for tomorrow haha

--- game-jams/snobbery-&-zombies/scripts/player.gd
+++ game-jams/snobbery-&-zombies/scripts/player.gd
@@ -2,10 +2,17 @@ extends CharacterBody2D
 
 @onready var pivot: Node2D = $Pivot_Anchor
 @onready var gun: AnimatedSprite2D = $Pivot_Anchor/AnimatedSprite2D
+var speed: float = 40.0
 
 var bullet_path = preload("res://scenes/bullet.tscn")
 
+func get_input():
+	var input_direction = Input.get_vector("move_left","move_right","move_up","move_down")
+	velocity = input_direction * speed
 func _physics_process(delta: float) -> void:
+	get_input()
+	
+	#set up gun rotation
 	var mouse_position = get_global_mouse_position()
 	pivot.look_at(mouse_position)
 	if Input.is_action_just_pressed("left_click"):
--- game-jams/snobbery-&-zombies/scripts/zombie.gd
+++ game-jams/snobbery-&-zombies/scripts/zombie.gd
@@ -1,6 +1,42 @@
 extends CharacterBody2D
-
 class_name Zombie
+
+@onready var player: CharacterBody2D = %Player
+@onready var nav_agent: NavigationAgent2D = %NavigationAgent2D
+
+var speed: float = 20.0
+var target_position: Vector2
+
+func _ready() -> void:
+	nav_agent.path_desired_distance = 1.0
+	nav_agent.target_desired_distance = 1.0
+	
+	actor_setup.call_deferred()
+
+func actor_setup():
+	# Wait for the first physics frame so the NavigationServer can sync.
+	await get_tree().physics_frame
+
+	# Now that the navigation map is no longer empty, set the movement target.
+	set_movement_target(target_position)
+
+func set_movement_target(movement_target: Vector2):
+	nav_agent.target_position = movement_target
+
+func _process(delta: float) -> void:
+	target_position = player.position
+	set_movement_target(target_position)
+
+func _physics_process(delta):
+	if nav_agent.is_navigation_finished():
+		return
+
+	var current_agent_position: Vector2 = global_position
+	var next_path_position: Vector2 = nav_agent.get_next_path_position()
+
+	velocity = current_agent_position.direction_to(next_path_position) * speed
+	move_and_slide()
+
 
 func hit():
 	queue_free()
