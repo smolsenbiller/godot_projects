ed36242ec093ba3bfe74772c872269d75ad8d57a
smolsenbiller <jacob.august.smith@icloud.com>
2026-02-11 15:57:50 -0700

    Started making a main menu.

--- 3d-platformer/scripts/finish_flag.gd
+++ 3d-platformer/scripts/finish_flag.gd
@@ -5,4 +5,8 @@ extends Area3D
 func _on_body_entered(body: Node3D) -> void:
 	if not body.is_in_group("Player"):
 		return
+	call_deferred("_load_scene")
+
+
+func _load_scene():
 	get_tree().change_scene_to_packed(next_level)
