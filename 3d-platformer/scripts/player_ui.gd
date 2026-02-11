76f332d20c51459abee0542e11282c6c8e18a820
smolsenbiller <jacob.august.smith@icloud.com>
2026-02-11 14:18:23 -0700

    Damage and heart containers are now connected.

--- 3d-platformer/scripts/player_ui.gd
+++ 3d-platformer/scripts/player_ui.gd
@@ -6,15 +6,21 @@ var hearts : Array = []
 @onready var score_text : Label = $ScoreText
 
 func _ready() -> void:
+	hearts = health_container.get_children()
+	
+	print(hearts)
+	
 	var player = get_parent()
 	
 	player.OnTakeDamage.connect(_update_hearts)
 	player.OnUpdateScore.connect(_update_score_text)
 	
+	_update_hearts(player.health)
 	_update_score_text(PlayerStats.score)
 
 func _update_hearts(health : int):
-	pass
+	for i in len(hearts):
+		hearts[i].visible = i < health
 	
 
 func _update_score_text(score: int):
