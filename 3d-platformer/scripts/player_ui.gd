aeb7c7047b875335d984781f8d65550dd341f2e8
smolsenbiller <jacob.august.smith@icloud.com>
2026-02-11 14:17:29 -0700

    I have added the scoring feature.

--- 3d-platformer/scripts/player_ui.gd
+++ 3d-platformer/scripts/player_ui.gd
@@ -1,1 +1,21 @@
 extends CanvasLayer
+
+@onready var health_container : HBoxContainer = $HBoxContainer
+var hearts : Array = []
+
+@onready var score_text : Label = $ScoreText
+
+func _ready() -> void:
+	var player = get_parent()
+	
+	player.OnTakeDamage.connect(_update_hearts)
+	player.OnUpdateScore.connect(_update_score_text)
+	
+	_update_score_text(PlayerStats.score)
+
+func _update_hearts(health : int):
+	pass
+	
+
+func _update_score_text(score: int):
+	score_text.text = "Score: " + str(score)
