a5682b318ccadbe0b6f966143615589f8d405269
smolsenbiller <jacob.august.smith@icloud.com>
2026-02-12 14:43:12 -0700

    Added basic moving and jumping controls.

--- walking-sim/scripts/PlayerController.gd
+++ walking-sim/scripts/PlayerController.gd
@@ -12,17 +12,44 @@ var is_running : bool = false
 
 @export_group("Camera")
 @export var look_sensitivity : float = 0.005
-var camera_look_inpur : Vector2
+var camera_look_input : Vector2
 
 @onready var camera : Camera3D = get_node("Camera3D")
 @onready var gravity : float = ProjectSettings.get_setting("physics/3d/default_gravity") * gravity_modifier
 
 
 func _ready() -> void:
-	pass
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 func _physics_process(delta: float) -> void:
-	pass
+	#Apply Gravity
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+	
+	if Input.is_action_pressed("jump") and is_on_floor():
+		velocity.y = jump_force 
+	
+	#Movement
+	var move_input = Input.get_vector("move_left", "move_right", "move_forward", "move_backward")
+	var move_dir = (transform.basis * Vector3(move_input.x, 0, move_input.y)).normalized()
+	
+	if Input.is_action_pressed("sprint"):
+		velocity.x = move_dir.x * max_run_speed
+		velocity.z = move_dir.z * max_run_speed
+	else:
+		velocity.x = move_dir.x * max_speed
+		velocity.z = move_dir.z * max_speed
+	
+	move_and_slide()
+	
+	
+	#camera look
+	rotate_y(-camera_look_input.x * look_sensitivity)
+	camera.rotate_x(-camera_look_input.y * look_sensitivity)
+	camera.rotation.x = clamp(camera.rotation.x, -1.5, 1.5)
+	camera_look_input = Vector2.ZERO
+
 
 func _unhandled_input(event: InputEvent) -> void:
-	pass
+	if event is InputEventMouseMotion:
+		camera_look_input = event.relative
