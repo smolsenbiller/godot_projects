20a0869a3ea2c1242215975a49ef22b63044b79c
smolsenbiller <jacob.august.smith@icloud.com>
2026-02-18 16:06:25 -0700

    Added the enemy AI so that they attack player units.

--- rts-game/scripts/unit_ai.gd
+++ rts-game/scripts/unit_ai.gd
@@ -1,11 +1,41 @@
 extends Node
 
+@export var detect_range : float = 100.0 #number in pixels
+@export var detect_rate : float = 0.2 #number in seconds
+var last_detect_time : float
+var enemy_list : Array[Unit] = []
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+@onready var unit : Unit = get_parent()
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	var time = Time.get_unix_time_from_system()
+	
+	if time - last_detect_time > detect_rate:
+		last_detect_time = time
+		_update_enemy_list()
+		_detect()
+
+func _update_enemy_list():
+	enemy_list.clear()
+	
+	var raw_list = get_tree().get_nodes_in_group("UnitPlayer")
+	
+	for node in raw_list:
+		if node is not Unit:
+			continue
+		
+		enemy_list.append(node)
+
+
+func _detect():
+	var closest_enemy = null
+	var closest_distance = 999999
+	
+	for enemy in enemy_list:
+		var dist = unit.global_position.distance_to(enemy.global_position)
+		if dist < closest_distance:
+			closest_enemy = enemy
+			closest_distance = dist
+	
+	if closest_enemy != null:
+		unit.set_attack_target(closest_enemy)
